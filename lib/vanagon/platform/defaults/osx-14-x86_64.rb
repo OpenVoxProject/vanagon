f8941a001a25d137bbaefbb37c204af66a60755a
Shaun Rutherford <srutherford@blizzard.com>
2025-07-29 11:23:04 -0500

    Added: lib/vanagon/platform/defaults/osx-14-x86_64.rb
    Chown for MacOS directories

--- lib/vanagon/platform/defaults/osx-14-x86_64.rb
+++ lib/vanagon/platform/defaults/osx-14-x86_64.rb
@@ -15,15 +15,18 @@ platform "osx-14-x86_64" do |plat|
   plat.provision_with "sudo dscl . -passwd /Users/test password"
   plat.provision_with "sudo dscl . -merge /Groups/admin GroupMembership test"
   plat.provision_with "echo 'test ALL=(ALL:ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/username > /dev/null"
-  plat.provision_with "sudo mkdir -p /etc/homebrew"
-  plat.provision_with "cd /etc/homebrew"
+  #plat.provision_with "sudo mkdir -p /etc/homebrew"
+  #plat.provision_with "cd /etc/homebrew"
   plat.provision_with "sudo createhomedir -c -u test"
   if File.directory?("/usr/local/var/homebrew")
     plat.provision_with "sudo chown -R test /usr/local/var/homebrew /usr/local/share/zsh /usr/local/share/zsh/site-functions \
     /usr/local/etc/bash_completion.d /usr/local/lib/pkgconfig /usr/local/share/aclocal /usr/local/share/locale"
+
+    # MacOS changing ownership of the files inside the directories, but not the directory ownership itself
+    plat.provision_with "sudo chown test /usr/local/var/homebrew /usr/local/share/zsh /usr/local/share/zsh/site-functions \
+    /usr/local/etc/bash_completion.d /usr/local/lib/pkgconfig /usr/local/share/aclocal /usr/local/share/locale"
   else
-    puts "testing"
-    #plat.provision_with %Q(sudo su test -c 'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+    plat.provision_with %Q(sudo su test -c 'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
   end
   plat.vmpooler_template "macos-14-x86_64"
 end
