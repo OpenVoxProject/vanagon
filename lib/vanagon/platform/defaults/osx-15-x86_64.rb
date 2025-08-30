a6b99f99e770f11dc53063ec4dcdce4ed034a1be
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-08-30 10:03:20 -0700

    Nick's changes
    A bit of cleanup to not chown everything

--- lib/vanagon/platform/defaults/osx-15-x86_64.rb
+++ lib/vanagon/platform/defaults/osx-15-x86_64.rb
@@ -18,15 +18,7 @@ platform "osx-15-x86_64" do |plat|
   plat.provision_with "sudo mkdir -p /etc/homebrew"
   plat.provision_with "cd /etc/homebrew"
   plat.provision_with "sudo createhomedir -c -u test"
-  if File.directory?("/usr/local/var/homebrew")
-    plat.provision_with "sudo chown -R test /usr/local/var/homebrew /usr/local/share/zsh /usr/local/share/zsh/site-functions \
-    /usr/local/etc/bash_completion.d /usr/local/lib/pkgconfig /usr/local/share/aclocal /usr/local/share/locale /usr/local/include /usr/local/lib"
-
-    # MacOS changing ownership of the files inside the directories, but not the directory ownership itself
-    plat.provision_with "sudo chown test /usr/local/var/homebrew /usr/local/share/zsh /usr/local/share/zsh/site-functions \
-    /usr/local/etc/bash_completion.d /usr/local/lib/pkgconfig /usr/local/share/aclocal /usr/local/share/locale"
-  else
-    plat.provision_with %Q(sudo su test -c 'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-  end
+  plat.provision_with %Q(sudo su test -c 'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"') unless File.directory?('/usr/local/var/homebrew')
+  plat.provision_with "sudo chown -R test /usr/local"
   plat.vmpooler_template "macos-15-x86_64"
 end
